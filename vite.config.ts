9be7ebdfb383006f7857f3adb73c630f7dc792ee
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-10-24 20:09:00 +1300

    chore: include type declarations

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from "node:path";
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
